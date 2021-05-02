5749e0a103387e44c83ec52cddb358a6238521ee
Nicholas Yue <yue.nicholas@gmail.com>
2021-05-02 10:45:14 -0700

    Clean up

--- Qt5/language_change/mainwindow/MainWindow.h
+++ Qt5/language_change/mainwindow/MainWindow.h
@@ -15,19 +15,14 @@ class MainWindow : public QMainWindow
     Q_OBJECT
     //
     typedef QSharedPointer<QTranslator> TranslatorShdPtr;
-    // typedef QMap<QString,TranslatorShdPtr> LanguageTranslatorMapContainer;
     struct abc {
         QString name;
         QString bcp47;
         TranslatorShdPtr translator;
     };
-
     typedef QVector<abc> LanguageTranslatorVecContainer;
 public:
     MainWindow(QWidget *parent = 0);
-protected:
-	// this event is called, when a new translator is loaded or the system language is changed
-	// void changeEvent(QEvent*);
 private:
 	QCheckBox* ui_checkBox;
 	QLabel* ui_label;
