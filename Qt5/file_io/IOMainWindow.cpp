bdddf17672c2bb0d2de6bb89c985be1759ba7423
Nicholas Yue <yue.nicholas@gmail.com>
2021-05-15 21:18:47 -0700

    Fix FileOpen dialog on WASM build

--- Qt5/file_io/IOMainWindow.cpp
+++ Qt5/file_io/IOMainWindow.cpp
@@ -20,8 +20,16 @@ IOMainWindow::~IOMainWindow()
 
 void IOMainWindow::OnFileOpen()
 {
-    QString fileName = QFileDialog::getOpenFileName(this,
-                                                    tr("Open Image"), "/tmp", tr("Image Files (*.png *.jpg *.bmp)"));
+	// The recommend way to open file when using it in WASM
+	// https://doc.qt.io/qt-5/qfiledialog.html#getOpenFileContent
+    auto fileContentReady = [](const QString &fileName, const QByteArray &fileContent) {
+        if (fileName.isEmpty()) {
+            // No file was selected
+        } else {
+            // Use fileName and fileContent
+        }
+    };
+    QFileDialog::getOpenFileContent("Images (*.png *.xpm *.jpg)",  fileContentReady);
 }
 
 void IOMainWindow::OnFileClose()
