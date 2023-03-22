6988ee300b0d18b81fd86b669dd9058bde71a1fc
Nicholas Yue <yue.nicholas@gmail.com>
2023-03-22 10:14:55 -0700

    Add boost library linkage to test macdeployqt

--- Qt5/packaging_and_installation/util.cpp
+++ Qt5/packaging_and_installation/util.cpp
@@ -1,6 +1,14 @@
 #include "util.h"
+#include <iostream>
+#include <boost/format.hpp>
+#include <boost/dll/runtime_symbol_info.hpp>
+#include <boost/filesystem.hpp>
 
 int myfunction() {
+    boost::filesystem::path prog_loc = boost::dll::program_location();
+    boost::filesystem::path default_plugin_dir = prog_loc.parent_path();
+
+	std::cout << boost::format("myfunction() called from '%1%'") % prog_loc.parent_path() << std::endl;
 	int i=42;
 	return i;
 }
