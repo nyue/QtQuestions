d539015a08206cc51556aef0bf8386749c4dc09d
Nicholas Yue <yue.nicholas@gmail.com>
2023-09-12 21:12:54 -0700

    macos menu question

---
+++ PySide2/macos_menu_about/MainWindow.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'main_window.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.actionOpen = QAction(MainWindow)
+        self.actionOpen.setObjectName(u"actionOpen")
+        self.actionSave = QAction(MainWindow)
+        self.actionSave.setObjectName(u"actionSave")
+        self.actionSave_As = QAction(MainWindow)
+        self.actionSave_As.setObjectName(u"actionSave_As")
+        self.actionQuit = QAction(MainWindow)
+        self.actionQuit.setObjectName(u"actionQuit")
+        self.actionAbout_Plugins = QAction(MainWindow)
+        self.actionAbout_Plugins.setObjectName(u"actionAbout_Plugins")
+        self.actionAbout_PyGraphUI = QAction(MainWindow)
+        self.actionAbout_PyGraphUI.setObjectName(u"actionAbout_PyGraphUI")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 22))
+        self.menuFile = QMenu(self.menubar)
+        self.menuFile.setObjectName(u"menuFile")
+        self.menuHelp = QMenu(self.menubar)
+        self.menuHelp.setObjectName(u"menuHelp")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuHelp.menuAction())
+        self.menuFile.addAction(self.actionOpen)
+        self.menuFile.addAction(self.actionSave)
+        self.menuFile.addAction(self.actionSave_As)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionQuit)
+        self.menuHelp.addAction(self.actionAbout_Plugins)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionOpen.setText(QCoreApplication.translate("MainWindow", u"Open", None))
+        self.actionSave.setText(QCoreApplication.translate("MainWindow", u"Save", None))
+        self.actionSave_As.setText(QCoreApplication.translate("MainWindow", u"Save As...", None))
+        self.actionQuit.setText(QCoreApplication.translate("MainWindow", u"Quit", None))
+        self.actionAbout_Plugins.setText(QCoreApplication.translate("MainWindow", u"About Plugins", None))
+        self.actionAbout_PyGraphUI.setText(QCoreApplication.translate("MainWindow", u"About PyGraphUI", None))
+        self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
+        self.menuHelp.setTitle(QCoreApplication.translate("MainWindow", u"Help", None))
+    # retranslateUi
+
---
+++ PySide2/macos_menu_about/main.py
@@ -0,0 +1,21 @@
+import sys
+from PySide2 import QtWidgets
+from MainWindow import Ui_MainWindow
+
+
+class MyMainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MyMainWindow, self).__init__()
+        self.setupUi(self)
+
+        self.actionQuit.triggered.connect(QtWidgets.QApplication.instance().quit)
+        self.actionAbout_Plugins.triggered.connect(self.about_Plugins)
+
+    def about_Plugins(self):
+        print('Plugins')
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = MyMainWindow()
+window.show()
+app.exec_()
